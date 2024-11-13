fbcc2a2eaf7f029d74f671f7d1558bf6753c545e
qtc789 <qiantongci@126.com>
2024-11-13 23:05:06 +0800

    Add files via upload

---
+++ 4.3/4.3.cpp
@@ -0,0 +1,23 @@
+﻿// 4.3.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    const int ArSize = 20;
+    char name[ArSize];
+    char dessert[ArSize];
+
+    cout << "Enter your name;\n";
+    cin>> name;
+    cout << "Enter your favorite dessert;\n";
+    cin >> dessert;
+    cout << "I have some delicious " << dessert;
+    cout << " for you," << name << " .\n";
+
+    return 0;
+}
+
+
